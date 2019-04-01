9e649fff0e7ff014bace18646f837fb13fdc1188
sergei kuksov <sergey.kuksov@func.ru>
2019-04-02 00:53:51 +0300

    Добавил imgmin

--- gulpfile.js/img.js
+++ gulpfile.js/img.js
@@ -2,10 +2,14 @@
 const { src, dest } = require('gulp');
 const plumber = require('gulp-plumber');
 const config = require('./config');
+const imagemin = require('gulp-imagemin');
 
 const img = cb => {
   src(config.src.img)
     .pipe(plumber(config.notify))
+    .pipe(imagemin({
+      verbose: true
+    }))
     .pipe(dest(config.dest.img));
 
   cb();
